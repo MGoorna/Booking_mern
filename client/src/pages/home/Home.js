25eec06f4dfb6492ed46b63fd71124b716543ff5
magda <m.goorna@gmail.com>
2022-10-05 13:34:50 +0200

    footer

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,10 +1,11 @@
-import Featured from '../../components/featured/Featured'
-import Navbar from '../../components/navbar/Navbar'
-import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
+import Featured from '../../components/featured/Featured';
 import FeatureProperties from '../../components/featureProperties/FeatureProperties';
-import './home.css';
 import FeatureExplore from '../../components/featureExplore/FeatureExplore';
 import MailList from '../../components/mailList/MailList';
+import Footer from '../../components/footer/Footer';
+import './home.css';
 
 const Home = () => {
   return ( <>
@@ -12,24 +13,17 @@ const Home = () => {
     <Header />   
     <div className="home">
       <div className="h__container">
-
         <h2 className="h__title">Homes guests love</h2>
-        <Featured />
-        
+        <Featured />      
         <h2 className="h__title">Explore Switzerland</h2>
         <p className="h__title">These popular destinations have a lot to offer</p>
         <FeatureExplore />
-
         <h2 className="h__title">Browse by property type</h2>
         <FeatureProperties />
-
         <MailList />
-
+        <Footer />
       </div>
     </div>
-
-
-    <div className="App-header"></div>
   </> );
 }
  

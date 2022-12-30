6113f8fd5e66b28084b4ce5f8a40ee74b175019e
magda <m.goorna@gmail.com>
2022-12-30 21:50:13 +0100

    rental sections

--- client/src/pages/rental/Rental.js
+++ client/src/pages/rental/Rental.js
@@ -1,22 +1,26 @@
+import FeatureProperties from '../../components/featureProperties/FeatureProperties';
+import FeatureExplore from '../../components/featureExplore/FeatureExplore';
+
 const Rental = () => {
   return ( 
     <>
       <h1>Car rentals for any kind of trip</h1>
       <h5>Compare deals from the biggest car rental companies</h5>
-      <div>
+      <section>
         <input type="radio" id="same-location"/>
         <label htmlFor="">Return to same location</label>
         <input type="radio" id="different-location"/>
-        <label htmlFor="">Return to different location</label>
-        
+        <label htmlFor="">Return to different location</label>  
         <div>
           <input type="checkbox" id="driver-age"/>
           <label htmlFor="driver-age">Driver aged between 30 - 65 </label>
         </div>
-
-      </div>
-
-      <div>
+      </section>
+      <section>
+        <h2>Popular destinations for car rentals</h2>
+        <FeatureExplore />
+      </section>
+      <section>
         <h2>Popular rental car companies</h2>
         <ul>
           <li>
@@ -32,8 +36,11 @@ const Rental = () => {
             <div className="">Avis</div>
           </li>
         </ul>
-        
-      </div>
+      </section>
+      <section>
+        <h2>Top worldwide locations for car rentals</h2>
+        <FeatureProperties />
+      </section>
     </>
    );
 }
